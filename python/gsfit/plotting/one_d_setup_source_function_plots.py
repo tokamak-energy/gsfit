fa72f109c1c8b7b189d693f6b0d308d41e9b3f5b
Peter Buxton <peter.buxton@tokamakenergy.co.uk>
2026-04-11 08:32:53 +0100

    fixed mypy error

--- python/gsfit/plotting/one_d_setup_source_function_plots.py
+++ python/gsfit/plotting/one_d_setup_source_function_plots.py
@@ -25,7 +25,7 @@ def plot() -> tuple[matplotlib.figure.Figure, list[matplotlib.axes.Axes]]:
 
     # Auto-refresh the legend on ax[1] whenever the figure is redrawn,
     # so that any new labeled artists added after setup appear automatically.
-    def _auto_legend(event, _updating=[False]):
+    def _auto_legend(event: object, _updating: list[bool] = [False]) -> None:
         # Guard against infinite recursion: legend() triggers a draw,
         # which would re-enter this callback without the guard.
         if _updating[0]:
