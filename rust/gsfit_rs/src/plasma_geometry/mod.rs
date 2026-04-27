97ac94693ff1522407dec15aa79cd37384b93523
Peter Buxton <peter.buxton@tokamakenergy.co.uk>
2026-04-28 00:42:56 +0100

    wip

--- rust/gsfit_rs/src/plasma_geometry/mod.rs
+++ rust/gsfit_rs/src/plasma_geometry/mod.rs
@@ -26,6 +26,7 @@ pub use find_magnetic_axis::MagneticAxis;
 pub use find_magnetic_axis::find_magnetic_axis;
 pub use find_stationary_points::StationaryPoint;
 pub use find_stationary_points::find_stationary_points;
+pub use find_stationary_points_using_full_quadrant_method::find_stationary_points_using_full_quadrant_method;
 pub use find_viable_xpt::find_viable_xpt;
 pub use flood_fill_mask::flood_fill_mask;
 pub use hessian::hessian;
