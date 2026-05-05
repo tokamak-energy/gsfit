cc3a14f6b942d016d4b1d30d6fed9c4939883104
Peter Buxton <peter.buxton@tokamakenergy.com>
2026-05-05 07:37:07 +0100

    wip

--- rust/gsfit_rs/src/greens/mod.rs
+++ rust/gsfit_rs/src/greens/mod.rs
@@ -6,6 +6,8 @@ mod greens_d2_psi_d_r2;
 mod greens_d_b_d_z;
 mod greens_psi;
 mod mutual_inductance_finite_size_to_finite_size;
+mod mutual_inductance_finite_size_to_point;
+mod greens;
 
 // Expose functions to public
 pub use d2_psi_d_r2_calculator::D2PsiDR2Calculator;
