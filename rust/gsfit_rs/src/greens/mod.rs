c901d174d45242c4ee806ce1ec45c1a9e3a6df18
Peter Buxton <peter.buxton@tokamakenergy.co.uk>
2026-05-08 22:17:41 +0100

    exposed greens_psi to pass doctest

--- rust/gsfit_rs/src/greens/mod.rs
+++ rust/gsfit_rs/src/greens/mod.rs
@@ -11,4 +11,4 @@ pub use d2_psi_d_r2_calculator::D2PsiDR2Calculator;
 pub use filament_geometry::FilamentGeometry;
 pub use greens::Greens;
 pub use mutual_inductance_finite_size_to_finite_size::mutual_inductance_finite_size_to_finite_size;
-pub use greens_psi::greens_py;
+pub use greens_psi::{greens_py, greens_psi};
